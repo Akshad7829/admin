bbbf1f45d81f0ed88a7408aa6edd1b12e1339f2f
Akshad7829 <akshadsn03@gmail.com>
2021-07-07 10:20:54 +0530

    file added

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import * as React from "react";
+import { Admin, Resource, EditGuesser } from 'react-admin';
+import { PostList } from './posts';
+import { UserList } from './users';
+
+import jsonServerProvider from 'ra-data-json-server';
+
+const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
+
+const App = () => (
+   <Admin dataProvider={dataProvider}>
+      
+      <Resource name="posts" list={PostList} edit={EditGuesser} />
+       <Resource name="users" list={UserList} />
+  </Admin>
+);
+
+export default App;
