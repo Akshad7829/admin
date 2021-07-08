ca81b3b532298fd41873d115fac16a0f8c954504
Akshad Nayakwadi <akshadsn03@gmail.com>
2021-07-08 17:45:50 +0530

    changes in files

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import * as React from "react";
-import { Admin, Resource, EditGuesser } from 'react-admin';
-import { PostList } from './posts';
+import { Admin, Resource } from 'react-admin';
+import { PostList, PostEdit, PostCreate } from './posts';
 import { UserList } from './users';
-
 import jsonServerProvider from 'ra-data-json-server';
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 
 const App = () => (
-   <Admin dataProvider={dataProvider}>
-      
-      <Resource name="posts" list={PostList} edit={EditGuesser} />
-       <Resource name="users" list={UserList} />
-  </Admin>
+    <Admin dataProvider={dataProvider}>
+    <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} />
+<Resource name="users" list={UserList} />
+    </Admin>
 );
 
 export default App;
